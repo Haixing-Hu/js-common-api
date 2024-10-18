796c51c108eaba0a73b2419eb5f7bd662b8c3ce2
Haixing Hu <starfish.hu@gmail.com>
2024-10-18 12:49:27 +0800

    feat: 提供管理`Upload`对象的API

---
+++ src/upload.js
@@ -0,0 +1,164 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2024.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+import { http } from '@haixing_hu/common-app';
+import { stringifyId, toJSON } from '@haixing_hu/common-decorator';
+import {
+  Upload,
+  Page,
+  PageRequest,
+} from '@haixing_hu/common-model';
+import { loading } from '@haixing_hu/common-ui';
+import { checkArgumentType } from '@haixing_hu/common-util';
+import { Log, Logger } from '@haixing_hu/logging';
+import { assignOptions, toJsonOptions } from './impl/options';
+
+const logger = Logger.getLogger('UploadApi');
+
+/**
+ * 提供管理`Upload`对象的API。
+ *
+ * @author 胡海星
+ */
+class UploadApi {
+  /**
+   * 列出符合条件的`Upload`对象。
+   *
+   * @param {PageRequest} pageRequest
+   *     分页请求。
+   * @param {object} criteria
+   *     查询条件参数，所有条件之间用`AND`连接。允许的条件包括：
+   *     - `type: string` 附件类型；
+   *     - `deleted: boolean` 是否已经被标记删除；
+   *     - `createTimeStart: string`创建时间范围的（闭区间）起始值；
+   *     - `createTimeEnd: string` 创建时间范围的（闭区间）结束值；
+   *     - `modifyTimeStart: string` 修改时间范围的（闭区间）起始值；
+   *     - `modifyTimeEnd: string` 修改时间范围的（闭区间）结束值；
+   *     - `deleteTimeStart: string` 标记删除时间范围的（闭区间）起始值；
+   *     - `deleteTimeEnd: string` 标记删除时间范围的（闭区间）结束值；
+   * @param {object} sort
+   *     - `sortField: string` 用于排序的属性名称（CamelCase形式）；
+   *     - `sortOrder: SortOrder` 指定是正序还是倒序。
+   * @return {Promise<Page<Upload>>}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回一个`Page`对象，包含符合条
+   *     件的`Upload`对象的分页数据；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  list(pageRequest, criteria, sort) {
+    checkArgumentType('pageRequest', pageRequest, [PageRequest, Object]);
+    checkArgumentType('criteria', criteria, [Object]);
+    checkArgumentType('sort', sort, [Object], true);
+    const params = toJSON({
+      ...pageRequest,
+      ...criteria,
+      ...sort,
+    }, toJsonOptions);
+    loading.showGetting();
+    return http.get('/upload', {
+      params,
+    }).then((data) => {
+      const page = Page.create(data, assignOptions);
+      page.content = Upload.createArray(page.content, assignOptions);
+      logger.info('Successfully list the Upload.');
+      logger.debug('The page of Upload is:', page);
+      return page;
+    });
+  }
+
+  /**
+   * 获取指定的`Upload`对象。
+   *
+   * @param {string|number|bigint} id
+   *     `Upload`对象的ID。
+   * @return {Promise<Upload>}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回指定的`Upload`对象；
+   *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  get(id) {
+    checkArgumentType('id', id, [String, Number, BigInt]);
+    loading.showGetting();
+    return http.get(`/upload/${stringifyId(id)}}`).then((data) => {
+      const upload = Upload.create(data, assignOptions);
+      logger.info('Successfully get the Upload by ID:', id);
+      logger.debug('The Upload is:', upload);
+      return upload;
+    });
+  }
+
+  /**
+   * 根据ID，标记删除一个`Upload`对象。
+   *
+   * @param {string} id
+   *     要标记删除的`Upload`对象的ID。
+   * @return {Promise<string>}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回数据被标记删除的UTC时间戳，
+   *     以ISO-8601格式表示为字符串；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  delete(id) {
+    checkArgumentType('id', id, [String, Number, BigInt]);
+    loading.showDeleting();
+    return http.delete(`/upload/${stringifyId(id)}`).then((timestamp) => {
+      logger.info('Successfully delete the Upload by ID %s at:', id, timestamp);
+      return timestamp;
+    });
+  }
+
+  /**
+   * 根据ID，恢复一个被标记删除的`Upload`对象。
+   *
+   * @param {string} id
+   *     要恢复的`Upload`对象的ID，该对象必须已经被标记删除。
+   * @return {Promise}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
+   *     则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  restore(id) {
+    checkArgumentType('id', id, [String, Number, BigInt]);
+    loading.showRestoring();
+    return http.patch(`/upload/${stringifyId(id)}`)
+      .then(() => logger.info('Successfully restore the Upload by ID:', id));
+  }
+
+  /**
+   * 根据ID，清除一个被标记删除的`Upload`对象。
+   *
+   * @param {string} id
+   *     要清除的`Upload`对象的ID，该对象必须已经被标记删除。
+   * @return {Promise}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
+   *     则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  purge(id) {
+    checkArgumentType('id', id, [String, Number, BigInt]);
+    loading.showPurging();
+    return http.delete(`/upload/${stringifyId(id)}/purge`)
+      .then(() => logger.info('Successfully purge the Upload by ID:', id));
+  }
+
+  /**
+   * 根彻底清除全部已被标记删除的`Upload`对象。
+   *
+   * @return {Promise}
+   *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
+   *     则解析失败并返回一个`ErrorInfo`对象。
+   */
+  @Log
+  purgeAll() {
+    loading.showPurging();
+    return http.delete('/upload/purge')
+      .then(() => logger.info('Successfully purge all deleted Upload.'));
+  }
+}
+
+const uploadApi = new UploadApi();
+
+export default uploadApi;
