e38793808f88224bb06bc493ab64619efd05a123
Haixing Hu <starfish.hu@gmail.com>
2024-10-20 21:55:43 +0800

    feat: export API interfaces

---
+++ src/index.js
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (c) 2022 - 2024.
+//    Haixing Hu, Qubit Co. Ltd.
+//
+//    All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+import appApi from './app';
+import appAuthenticateApi from './app-authenticate';
+import attachmentApi from './attachment';
+import categoryApi from './category';
+import cityApi from './city';
+import countryApi from './country';
+import currentUserApi from './current-user';
+import departmentApi from './department';
+import deviceApi from './device';
+import deviceInitApi from './device-init';
+import dictApi from './dict';
+import dictEntryApi from './dict-entry';
+import districtApi from './district';
+import employeeApi from './employee';
+import feedbackApi from './feedback';
+import fileApi from './file';
+import organizationApi from './organization';
+import personApi from './person';
+import provinceApi from './province';
+import settingApi from './setting';
+import socialNetworkAccountApi from './social-network-account';
+import streetApi from './street';
+import systemApi from './system';
+import uploadApi from './upload';
+import userAuthenticateApi from './user-authenticate';
+import verifyCodeApi from './verify-code';
+import wechatApi from './wechat';
+
+export {
+  appApi,
+  appAuthenticateApi,
+  attachmentApi,
+  categoryApi,
+  cityApi,
+  countryApi,
+  currentUserApi,
+  departmentApi,
+  deviceApi,
+  deviceInitApi,
+  dictApi,
+  dictEntryApi,
+  districtApi,
+  employeeApi,
+  feedbackApi,
+  fileApi,
+  organizationApi,
+  personApi,
+  provinceApi,
+  settingApi,
+  socialNetworkAccountApi,
+  streetApi,
+  systemApi,
+  uploadApi,
+  userAuthenticateApi,
+  verifyCodeApi,
+  wechatApi,
+};
