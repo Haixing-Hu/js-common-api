3fe2486f164e2438a0fed9ec9012279e8c08a522
Haixing Hu <starfish.hu@gmail.com>
2024-12-05 15:13:52 +0800

    refactor: 增加 showLoading 参数

--- src/api/app.js
+++ src/api/app.js
@@ -55,12 +55,14 @@ class AppApi {
    *     排序参数，指定按照哪个属性排序。允许的条件包括：
    *  - `sortField: string` 用于排序的属性名称（CamelCase形式）；
    *  - `sortOrder: SortOrder` 指定是正序还是倒序。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<Page<App>|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回一个`Page`对象，包含符合条
    *     件的`App`对象的分页数据；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  list(pageRequest = {}, criteria = {}, sort = {}) {
+  list(pageRequest = {}, criteria = {}, sort = {}, showLoading = true) {
     checkArgumentType('pageRequest', pageRequest, [PageRequest, Object]);
     checkArgumentType('criteria', criteria, Object);
     checkArgumentType('sort', sort, Object);
@@ -69,7 +71,9 @@ class AppApi {
       ...criteria,
       ...sort,
     }, toJsonOptions);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get('/app', {
       params,
     }).then((obj) => {
@@ -108,12 +112,14 @@ class AppApi {
    *     排序参数，指定按照哪个属性排序。允许的条件包括：
    *  - `sortField: string` 用于排序的属性名称（CamelCase形式）；
    *  - `sortOrder: SortOrder` 指定是正序还是倒序。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<Page<StatefulInfo>|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回一个`Page`对象，包含符合条
    *     件的`App`对象的基本信息的分页数据；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  listInfo(pageRequest = {}, criteria = {}, sort = {}) {
+  listInfo(pageRequest = {}, criteria = {}, sort = {}, showLoading = true) {
     checkArgumentType('pageRequest', pageRequest, [PageRequest, Object]);
     checkArgumentType('criteria', criteria, Object);
     checkArgumentType('sort', sort, Object);
@@ -122,7 +128,9 @@ class AppApi {
       ...criteria,
       ...sort,
     }, toJsonOptions);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get('/app/info', {
       params,
     }).then((obj) => {
@@ -138,14 +146,18 @@ class AppApi {
    *
    * @param {string|number|bigint} id
    *     `App`对象的ID。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<App|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回指定的`App`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  get(id) {
+  get(id, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get(`/app/${stringifyId(id)}`).then((obj) => {
       const result = App.create(obj, assignOptions);
       logger.info('Successfully get the App by ID:', id);
@@ -159,14 +171,18 @@ class AppApi {
    *
    * @param {string} code
    *     `App`对象的编码。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<App|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回指定的`App`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  getByCode(code) {
+  getByCode(code, showLoading = true) {
     checkArgumentType('code', code, String);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get(`/app/code/${code}`).then((obj) => {
       const result = App.create(obj, assignOptions);
       logger.info('Successfully get the App by code:', code);
@@ -180,14 +196,18 @@ class AppApi {
    *
    * @param {string|number|bigint} id
    *     `App`对象的ID。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<StatefulInfo|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回指定的`StatefulInfo`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  getInfo(id) {
+  getInfo(id, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get(`/app/${stringifyId(id)}/info`).then((obj) => {
       const result = StatefulInfo.create(obj, assignOptions);
       logger.info('Successfully get the info of the App by ID:', id);
@@ -201,14 +221,18 @@ class AppApi {
    *
    * @param {string} code
    *     `App`对象的编码。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<StatefulInfo|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回指定的`StatefulInfo`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  getInfoByCode(code) {
+  getInfoByCode(code, showLoading = true) {
     checkArgumentType('code', code, String);
-    loading.showGetting();
+    if (showLoading) {
+      loading.showGetting();
+    }
     return http.get(`/app/code/${code}/info`).then((obj) => {
       const result = StatefulInfo.create(obj, assignOptions);
       logger.info('Successfully get the info of the App by code:', code);
@@ -220,17 +244,21 @@ class AppApi {
   /**
    * 添加一个`App`对象。
    *
-   * @param {App} app
+   * @param {App|object} app
    *     要添加的`App`对象。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<App|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回新增的`App`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  add(app) {
-    checkArgumentType('app', app, App);
+  add(app, showLoading = true) {
+    checkArgumentType('app', app, [App, Object]);
     const data = toJSON(app, toJsonOptions);
-    loading.showAdding();
+    if (showLoading) {
+      loading.showAdding();
+    }
     return http.post('/app', data).then((obj) => {
       const result = App.create(obj, assignOptions);
       logger.info('Successfully add the App:', result.id);
@@ -242,18 +270,23 @@ class AppApi {
   /**
    * 根据ID，更新一个`App`对象。
    *
-   * @param {App} app
+   * @param {App|object} app
    *     要更新的`App`对象的数据，根据其ID确定要更新的对象。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<App|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回更新后的`App`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  update(app) {
-    checkArgumentType('app', app, App);
+  update(app, showLoading = true) {
+    checkArgumentType('app', app, [App, Object]);
+    checkArgumentType('app.id', app.id, [String, Number, BigInt]);
     const id = stringifyId(app.id);
     const data = toJSON(app, toJsonOptions);
-    loading.showUpdating();
+    if (showLoading) {
+      loading.showUpdating();
+    }
     return http.put(`/app/${id}`, data).then((obj) => {
       const result = App.create(obj, assignOptions);
       logger.info('Successfully update the App by ID %s at:', id, result.modifyTime);
@@ -267,15 +300,20 @@ class AppApi {
    *
    * @param {App} app
    *     要更新的`App`对象的数据，根据其编码确定要更新的对象。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<App|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回更新后的`App`对象；
    *     若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  updateByCode(app) {
+  updateByCode(app, showLoading = true) {
     checkArgumentType('app', app, App);
+    checkArgumentType('app.code', app.code, String);
     const data = toJSON(app, toJsonOptions);
-    loading.showUpdating();
+    if (showLoading) {
+      loading.showUpdating();
+    }
     return http.put(`/app/code/${app.code}`, data).then((obj) => {
       const result = App.create(obj, assignOptions);
       logger.info('Successfully update the App by code "%s" at:', result.code, result.modifyTime);
@@ -291,16 +329,20 @@ class AppApi {
    *     `App`对象的ID。
    * @param {State|string} state
    *     要更新的`App`对象的状态，必须是`State`枚举类型或表示其值的字符串。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<string|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回数据更新的UTC时间戳，
    *     以ISO-8601格式表示为字符串；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  updateState(id, state) {
+  updateState(id, state, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
     checkArgumentType('state', state, [State, String]);
     const data = { state: String(state) };
-    loading.showUpdating();
+    if (showLoading) {
+      loading.showUpdating();
+    }
     return http.put(`/app/${stringifyId(id)}/state`, data).then((timestamp) => {
       logger.info('Successfully update the state of the App by ID %s at:', id, timestamp);
       return timestamp;
@@ -314,16 +356,20 @@ class AppApi {
    *     要更新的`App`对象的编码。
    * @param {State|string} state
    *     要更新的`App`对象的状态，必须是`State`枚举类型或表示其值的字符串。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<string|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回数据更新的UTC时间戳，
    *     以ISO-8601格式表示为字符串；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  updateStateByCode(code, state) {
+  updateStateByCode(code, state, showLoading = true) {
     checkArgumentType('code', code, String);
     checkArgumentType('state', state, [State, String]);
     const data = { state: String(state) };
-    loading.showUpdating();
+    if (showLoading) {
+      loading.showUpdating();
+    }
     return http.put(`/app/code/${code}/state`, data).then((timestamp) => {
       logger.info('Successfully update the state of the App by code "%s" at:', code, timestamp);
       return timestamp;
@@ -335,14 +381,18 @@ class AppApi {
    *
    * @param {string} id
    *     要标记删除的`App`对象的ID。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<string|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回数据被标记删除的UTC时间戳，
    *     以ISO-8601格式表示为字符串；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  delete(id) {
+  delete(id, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
-    loading.showDeleting();
+    if (showLoading) {
+      loading.showDeleting();
+    }
     return http.delete(`/app/${stringifyId(id)}`).then((timestamp) => {
       logger.info('Successfully delete the App by ID %s at:', id, timestamp);
       return timestamp;
@@ -354,14 +404,18 @@ class AppApi {
    *
    * @param {string} code
    *     要标记删除的`App`对象的编码。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<string|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功并返回数据被标记删除的UTC时间戳，
    *     以ISO-8601格式表示为字符串；若操作失败，则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  deleteByCode(code) {
+  deleteByCode(code, showLoading = true) {
     checkArgumentType('code', code, String);
-    loading.showDeleting();
+    if (showLoading) {
+      loading.showDeleting();
+    }
     return http.delete(`/app/code/${code}`).then((timestamp) => {
       logger.info('Successfully delete the App by code "%s" at:', code, timestamp);
       return timestamp;
@@ -373,14 +427,18 @@ class AppApi {
    *
    * @param {string} id
    *     要恢复的`App`对象的ID，该对象必须已经被标记删除。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<void|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
    *     则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  restore(id) {
+  restore(id, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
-    loading.showRestoring();
+    if (showLoading) {
+      loading.showRestoring();
+    }
     return http.patch(`/app/${stringifyId(id)}`)
       .then(() => logger.info('Successfully restore the App by ID:', id));
   }
@@ -390,14 +448,18 @@ class AppApi {
    *
    * @param {string} code
    *     要恢复的`App`对象的编码，该对象必须已经被标记删除。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<void|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
    *     则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  restoreByCode(code) {
+  restoreByCode(code, showLoading = true) {
     checkArgumentType('code', code, String);
-    loading.showRestoring();
+    if (showLoading) {
+      loading.showRestoring();
+    }
     return http.patch(`/app/code/${code}`)
       .then(() => logger.info('Successfully restore the App by code:', code));
   }
@@ -407,14 +469,18 @@ class AppApi {
    *
    * @param {string} id
    *     要清除的`App`对象的ID，该对象必须已经被标记删除。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<void|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
    *     则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  purge(id) {
+  purge(id, showLoading = true) {
     checkArgumentType('id', id, [String, Number, BigInt]);
-    loading.showPurging();
+    if (showLoading) {
+      loading.showPurging();
+    }
     return http.delete(`/app/${stringifyId(id)}/purge`)
       .then(() => logger.info('Successfully purge the App by ID:', id));
   }
@@ -424,14 +490,18 @@ class AppApi {
    *
    * @param {string} code
    *     要清除的`App`对象的编码，该对象必须已经被标记删除。
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<void|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
    *     则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  purgeByCode(code) {
+  purgeByCode(code, showLoading = true) {
     checkArgumentType('code', code, String);
-    loading.showPurging();
+    if (showLoading) {
+      loading.showPurging();
+    }
     return http.delete(`/app/code/${code}/purge`)
       .then(() => logger.info('Successfully purge the App by code:', code));
   }
@@ -439,13 +509,17 @@ class AppApi {
   /**
    * 根彻底清除全部已被标记删除的`App`对象。
    *
+   * @param {boolean} showLoading
+   *     是否显示加载提示。
    * @return {Promise<void|ErrorInfo>}
    *     此HTTP请求的`Promise`对象。若操作成功，则解析成功且没有返回值；若操作失败，
    *     则解析失败并返回一个`ErrorInfo`对象。
    */
   @Log
-  purgeAll() {
-    loading.showPurging();
+  purgeAll(showLoading = true) {
+    if (showLoading) {
+      loading.showPurging();
+    }
     return http.delete('/app/purge')
       .then(() => logger.info('Successfully purge all deleted App.'));
   }
